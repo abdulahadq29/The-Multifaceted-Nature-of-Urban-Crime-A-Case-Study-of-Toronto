6a5e132c6c065fe873cb0d563ea8f2603a5d3b1b
Abdul Ahad Qureshi <ahad.qureshi@mail.utoronto.ca>
2024-01-25 07:55:26 +0000

    Add data download script for Arrested and Charged Persons dataset

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from opendatatoronto
+# Purpose: Automates the process of downloading a specific dataset from the Toronto Open Data portal.
 # Author: Abdul Ahad Qureshi
 # Date: 25 January 2024
 # Contact: ahad.qureshi@mail.utoronto.ca
@@ -8,18 +8,26 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
+library(opendatatoronto)  # For accessing Toronto Open Data
+library(tidyverse)        # For data manipulation and visualization
 
 
-#### Download data ####
+#### Data Retrieval Process ####
+# Fetching package information for the dataset of interest
+police_data_package <- opendatatoronto::show_package("police-annual-statistical-report-arrested-and-charged-persons")
 
+# Retrieving all available resources within the package
+police_data_resources <- opendatatoronto::list_package_resources("police-annual-statistical-report-arrested-and-charged-persons")
 
+# Filtering resources to include only certain formats (CSV, GeoJSON)
+valid_data_resources <- police_data_resources %>%
+  dplyr::filter(tolower(format) %in% c('csv', 'geojson'))
 
+# Obtaining the first resource from the filtered list as an example
+example_arrest_data <- dplyr::filter(valid_data_resources, dplyr::row_number() == 1) %>%
+  opendatatoronto::get_resource()
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+#### Saving the Retrieved Data ####
+# Saving the downloaded data into a CSV file
+# Ensure the directory 'inputs/data/' exists in your project
+readr::write_csv(example_arrest_data, "inputs/data/example_arrested_persons_data.csv")
