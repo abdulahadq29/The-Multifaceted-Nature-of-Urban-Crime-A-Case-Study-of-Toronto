750941b8e4cf7a64bd5ae894266a867af634b43e
Abdul Ahad Qureshi <ahad.qureshi@mail.utoronto.ca>
2024-01-25 06:05:18 +0000

    "Add simulation and analysis script for neighborhood ticketing data"

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,10 +10,27 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+library(ggplot2)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(123) # Setting seed for reproducibility
+n <- 1000 # Number of observations
+num_neighborhoods <- 10 # Total number of neighborhoods
+data <- data.frame(
+  Neighborhood = factor(sample(1:num_neighborhoods, n, replace = TRUE)),
+  TicketsIssued = rpois(n, lambda = 50) # Assuming Poisson distribution
+)
+# Basic data exploration
+summary(data)
+table(data$Neighborhood)
 
+# Visualization
+ggplot(data, aes(x = Neighborhood, y = TicketsIssued)) + 
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
+# Analyzing the correlation
+# Since Neighborhood is a factor, we'll use ANOVA to test for differences in ticket issuance among neighborhoods
+anova_result <- aov(TicketsIssued ~ Neighborhood, data = data)
+summary(anova_result)
